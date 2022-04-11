626accc30110dd0528b670bbee4c85f2c0e99327
Sup3r-Us3r <maydersonmello@gmail.com>
2022-04-10 21:09:08 -0300

    web base structure

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,21 @@
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+const App = () => {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <pre>
+        {JSON.stringify(user, null, 2)}
+      </pre>
+
+      <a href="/api/auth/logout">Logout</a>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired();
+
+export default App;
