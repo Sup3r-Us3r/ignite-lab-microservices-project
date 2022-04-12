7fa7cda9fe93d7d7067927c9252b822b7a563489
Sup3r-Us3r <maydersonmello@gmail.com>
2022-04-12 20:36:02 -0300

    create product resolver and product service

---
+++ purchases/src/http/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,22 @@
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { ProductsService } from 'src/services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
+import { Product } from '../models/product';
+
+@Resolver(() => Product)
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) {}
+
+  @Query(() => [Product])
+  products() {
+    return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
+  }
+}
