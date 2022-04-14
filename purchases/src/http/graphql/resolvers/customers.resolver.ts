d49fe54d6f8533a9cba77db47ddbc2d4e5c30d26
Sup3r-Us3r <maydersonmello@gmail.com>
2022-04-13 22:03:43 -0300

    create resolvers to purchases

---
+++ purchases/src/http/graphql/resolvers/customers.resolver.ts
@@ -0,0 +1,27 @@
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, ResolveField, Parent } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { CustomersService } from 'src/services/customers.service';
+import { PurchasesService } from 'src/services/purchases.service';
+import { Customer } from '../models/customer';
+import { Purchase } from '../models/purchase';
+
+@Resolver(() => Customer)
+export class CustomersResolver {
+  constructor(
+    private customersService: CustomersService,
+    private purchasesService: PurchasesService,
+  ) {}
+
+  @ResolveField(() => Purchase)
+  purchases(@Parent() customer: Customer) {
+    return this.purchasesService.listAllFromCustomer(customer.id);
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Query(() => Customer)
+  me(@CurrentUser() user: AuthUser) {
+    return this.customersService.getCustomerByAuthUserId(user.sub);
+  }
+}
--- purchases/src/http/graphql/resolvers/purchases.resolver.ts
+++ purchases/src/http/graphql/resolvers/purchases.resolver.ts
@@ -1,8 +1,18 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query, ResolveField, Parent } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  ResolveField,
+  Parent,
+  Mutation,
+  Args,
+} from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { CustomersService } from 'src/services/customers.service';
 import { ProductsService } from 'src/services/products.service';
 import { PurchasesService } from 'src/services/purchases.service';
+import { CreatePurchaseInput } from '../inputs/create-purchase-input';
 import { Product } from '../models/product';
 import { Purchase } from '../models/purchase';
 
@@ -11,9 +21,10 @@ export class PurchasesResolver {
   constructor(
     private purchaseService: PurchasesService,
     private productsService: ProductsService,
+    private customersService: CustomersService,
   ) {}
 
-  // @UseGuards(AuthorizationGuard)
+  @UseGuards(AuthorizationGuard)
   @Query(() => [Purchase])
   purchases() {
     return this.purchaseService.listAllPurchases();
@@ -22,5 +33,27 @@ export class PurchasesResolver {
   @ResolveField(() => Product)
   product(@Parent() purchase: Purchase) {
     return this.productsService.getProductById(purchase.productId);
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Purchase)
+  async createPurchase(
+    @Args('data') data: CreatePurchaseInput,
+    @CurrentUser() user: AuthUser,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      user.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: user.sub,
+      });
+    }
+
+    return this.purchaseService.createPurchase({
+      customerId: customer.id,
+      productId: data.productId,
+    });
   }
 }
